83a9537e69b2640b0132a3409e472dcc272ba3c6
Nicholas Ivey <nickivey@nicks-mbp.gateway.pace.com>
2016-01-19 13:53:02 -0500

    Submission

--- helloworld.py
+++ helloworld.py
@@ -8,3 +8,26 @@
 
 # make sure that your code contains comments explaining your logic!
 
+# Nicholas Ivey 1/19/2016
+
+print('Hello World') # print hello
+print("Choose a language and I'll greet you in that language!") #prompt for lang input
+print('1. Spanish')
+print('2. Italian')
+print('3. French')
+
+try:
+    num = int(raw_input())      # ask for input and store it as variable num
+except ValueError:
+    print('Invalid Entry')      # if 1,2,3 is not entered in print invalid
+
+if (num == 1):                  # if 1 entered reply in spanish
+        print('Hola')
+if (num == 2):                  # if 2 entered reply in italian
+        print('Ciao')
+if (num == 3):                  # if 3 entered reply in french
+        print('Salut')
+
+exit()
+
+
